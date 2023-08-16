fe7f864c92c9ac1b2e2a3e27d082b9e83b0ef176
huang yao <kirayao@hotmail.com>
2023-08-16 15:02:22 +0800

    feat: It can generate a website now

--- packages/generator/src/base.mjs
+++ packages/generator/src/base.mjs
@@ -1,48 +1,85 @@
 import fs from "fs/promises";
 
-export class DirViewer {
-  #path = "";
-  #opened = false;
+const { defineProperty } = Object;
 
+export class DirViewer extends Array {
   constructor(path) {
-    this.#path = path;
+    super();
+
+    defineProperty(this, "path", {
+      value: path,
+    });
+
+    defineProperty(this, "opened", {
+      value: (async () => {
+        await this.reload();
+      })(),
+    });
   }
 
-  async open() {
-    if (this.#opened) {
-      return;
-    }
+  async reload() {
+    const results = await fs.readdir(this.path);
 
-    const results = await fs.readdir(this.#path);
+    this.length = 0;
 
-    for (let e of results) {
-      if (/^\./.test(e) || e === "node_modules") {
-        continue;
-      }
-
-      const childPath = `${this.#path}/${e}`;
-
-      const fileStats = await fs.stat(childPath);
-
-      if (fileStats.isDirectory()) {
-        this[e] = new DirViewer(childPath);
-      } else {
-        this[e] = await fs.readFile(childPath, "utf-8");
-      }
-    }
-
-    this.#opened = true;
+    this.push(
+      ...results.filter((e) => !(/^\./.test(e) || e === "node_modules"))
+    );
   }
 
-  get names() {
-    return Object.keys(this);
+  async read(name) {
+    const childPath = `${this.path}/${name}`;
+    const fileStats = await fs.stat(childPath);
+
+    if (fileStats.isDirectory()) {
+      const reobj = new DirViewer(childPath);
+
+      await reobj.opened;
+
+      return reobj;
+    } else {
+      return await fs.readFile(childPath, "utf-8");
+    }
+  }
+
+  async write(name, content) {
+    await fs.writeFile(`${this.path}/${name}`, content);
+
+    await this.reload();
+  }
+
+  async mkdir(name) {
+    const path = `${this.path}/${name}`;
+
+    const result = await fs.stat(path).catch((err) => null);
+
+    if (result) {
+      await fs.rm(path, { recursive: true });
+    }
+
+    await fs.mkdir(path);
+
+    await this.reload();
+
+    return this.read(name);
+  }
+
+  async rm(name) {
+    await fs.rm(`${this.path}/${name}`, { recursive: true });
+
+    await this.reload();
+  }
+
+  get name() {
+    return this.path.replace(/.+\/(.+)/, "$1");
   }
 }
 
 // 底部兼容层，方便以后迁移到浏览器
 export async function readDir(path) {
   const viewer = new DirViewer(path);
-  await viewer.open();
+
+  await viewer.opened;
 
   return viewer;
 }
--- packages/generator/src/main.mjs
+++ packages/generator/src/main.mjs
@@ -1,15 +1,47 @@
 import { readDir } from "./base.mjs";
+import transConfig from "./transCOnfig.mjs";
+import transMD from "./transMD.mjs";
 
 globalThis.readDir = readDir;
 
 // 文档入口目录
 const entryDir = "/Users/huangyao/Documents/GitHub/ofa-v4-docs/packages/docs";
 
-(async () => {
-  const dirData = await readDir(entryDir);
+// 文档输出目录
+const outDir = "/Users/huangyao/Documents/GitHub/ofa-v4-docs/packages/website";
+
+// 初始操作
+const init = async () => {
+  const entryViewer = await readDir(entryDir);
+  const outPutViewer = await readDir(outDir);
 
   // 第一层只获取目录，判断内部有 config 的情况下，为一个语言的文档网站
-  console.log("dirData => ", dirData);
-})();
+  entryViewer.forEach(async (name) => {
+    const inputer = await entryViewer.read(name);
+    if (inputer.name && inputer.name !== "publics" && inputer.name !== "dist") {
+      const outputer = await outPutViewer.mkdir(name);
 
+      distTo(inputer, outputer);
+    }
+  });
+};
+
+globalThis.init = init;
+
+// 输出文件到指定目录
+const distTo = async (inputer, outputer) => {
+  let configData = await inputer.read("config.json");
+
+  if (!configData) {
+    return;
+  }
+
+  const docConfig = await transConfig(JSON.parse(configData), inputer);
+
+  await outputer.write("doc-config.json", JSON.stringify(docConfig));
+
+  transMD(inputer, outputer, "doc-config.json");
+};
+
+setTimeout(init, 2000);
 setTimeout(() => {}, 10000000);
