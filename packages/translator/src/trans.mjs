2378030d9e39224793f07017a8bb94c88b5a7ff9
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-06 19:52:25 +0800

    feat: complete the trans

--- packages/translator/src/trans.mjs
+++ packages/translator/src/trans.mjs
@@ -1,23 +1,66 @@
 import crypto from "crypto";
 import { marked } from "marked";
-import { readDir, write } from "../../generator/src/base.mjs";
+import { readDir, write } from "../../BaseReader/index.mjs";
 import setting from "./setting.mjs";
 import fanyi from "./fanyi.mjs";
 
 const { entry, caches, needs } = setting;
 
 const init = async () => {
-  const cacheHander = await readDir(caches);
+  const cachesReader = await readDir(caches);
 
   for (let lang of needs) {
+    const langCaches = await cachesReader.read(lang, true);
+
+    const names = await langCaches.names();
+
     // 将原文转换成段落哈希和哈希文章
-    const { hashObj, articles } = await getHashLineData(await readDir(entry));
+    const { hashObj } = await getHashLineData(await readDir(entry));
 
-    for (let [hash, content] of Object.entries(hashObj)) {
-      const tContent = await fanyi(content, lang);
+    const hashEntries = Object.entries(hashObj);
 
-      debugger;
+    const total = hashEntries.length;
+
+    // 将已存在的翻译剔除
+    const needTrans = hashEntries.filter((e) => !names.includes(e[0]));
+
+    let count = total - needTrans.length;
+
+    // 将所有文案进行分组，保持一组的文案在1k内
+    // 确保每次翻译的字符串在 1k 左右
+    let cacheObj = {};
+    for (let i = 0, len = needTrans.length; i < len; i++) {
+      const [hash, content] = needTrans[i];
+      cacheObj[hash] = content;
+
+      // 判断是否超过1k
+      const nowLen = JSON.stringify(Object.values(cacheObj));
+
+      if (nowLen.length > 1000 || i === len - 1) {
+        const ens = Object.entries(cacheObj);
+
+        const afterArr = await fanyi(
+          ens.map((e) => e[1]),
+          lang
+        );
+
+        await Promise.all(
+          ens.map(async ([hash], index) => {
+            const result = afterArr[index];
+
+            await langCaches.write(hash, result);
+            count++;
+          })
+        );
+
+        cacheObj = {};
+
+        console.clear();
+        console.log(`翻译 ${lang} 中：${count} / ${total}`);
+      }
     }
+
+    console.log(`翻译 ${lang} 完成`);
   }
 };
 
@@ -31,7 +74,8 @@ const getHashLineData = async (
   hashObj = {},
   articles = {}
 ) => {
-  for (let name of inputer) {
+  const names = await inputer.names();
+  for (let name of names) {
     const data = await inputer.read(name);
 
     if (data.name) {
