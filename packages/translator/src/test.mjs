6e3383f923999a3f6610ba464ef4f0727bf13b1d
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-07 19:16:19 +0800

    Update test.mjs

--- packages/translator/src/test.mjs
+++ packages/translator/src/test.mjs
@@ -1,7 +1,12 @@
 import { chat } from "./fanyi.mjs";
 
 async function main() {
-  console.log("chat: ", await chat("你是谁？"));
+  const arr = [
+    "**ofa.js** 符合了微前端的五个核心特点：",
+    "1. **独立部署**：每个组件和页面都可以独立地开发、测试和部署，这使得团队能够更迅速地发布新功能和解决问题。",
+  ];
+
+  console.log("chat: ", await chat(`将数组内的 markdown 翻译成英文：${JSON.stringify(arr)}`));
 }
 
 globalThis.test = main;
