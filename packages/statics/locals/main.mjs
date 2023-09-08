a5d89efc353033f7edcda5be96c7fd847b5d9edb
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-08 20:28:47 +0800

    feat: update ofa.js

---
+++ packages/statics/locals/main.mjs
@@ -0,0 +1,58 @@
+import { lang } from "../data.mjs";
+
+const en = {
+  search: "Search",
+  recent: "Recent",
+  norecent: "No recent search",
+  noresult: 'No results for "{{stext}}"',
+  inpreview: "In Preview",
+  preview: "Preview",
+  tabopen: "New tab opens",
+  refresh: "Refresh",
+};
+
+const cn = {
+  search: "搜索",
+  recent: "最近",
+  norecent: "没有搜索记录",
+  noresult: '没有搜索到关于"{{stext}}"的结果',
+  inpreview: "预览中",
+  preview: "预览网页",
+  tabopen: "新标签打开",
+  refresh: "刷新页面",
+};
+
+const tw = {
+  search: "搜索",
+  recent: "最近",
+  norecent: "没有搜索紀錄",
+  noresult: '沒有搜尋到關於"{{stext}}"的結果',
+  inpreview: "預覽中",
+  preview: "預覽網頁",
+  tabopen: "新標籤打開",
+  refresh: "刷新頁面",
+};
+
+const locals = $.stanz(en);
+
+locals._t = (key, data) => {
+  let target = locals.get(key);
+
+  Object.entries(data).forEach(([key, value]) => {
+    const reg = new RegExp(`\{\{ *${key}? *\}\}`, "g");
+    target = target.replace(reg, value);
+  });
+
+  return target;
+};
+
+switch (lang) {
+  case "cn":
+    Object.assign(locals, cn);
+    break;
+  case "t-cn":
+    Object.assign(locals, tw);
+    break;
+}
+
+export default locals;
