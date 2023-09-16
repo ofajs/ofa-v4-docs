337f1655f73ae6003aff00ece683f72f2360c649
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-09-16 14:03:03 +0800

    refactor: update o-book version

--- packages/docs/sw.js
+++ packages/docs/sw.js
@@ -1,2 +1,2 @@
-importScripts(`https://cdn.jsdelivr.net/npm/o-book@2.0.7/src/sw/base.js`);
+importScripts(`https://cdn.jsdelivr.net/npm/o-book@2.0.8/src/sw/base.js`);
 // importScripts(`http://127.0.0.1:5512/src/sw/base.js`);
