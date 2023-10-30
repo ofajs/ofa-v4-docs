6b770f93d2f44c63c5cabc8d637642b0454ce299
huang yao <5945154+kirakiray@users.noreply.github.com>
2023-10-30 18:27:59 +0800

    feat: update obool

--- docs/doc-sw.js
+++ docs/doc-sw.js
@@ -1,2 +1,2 @@
-importScripts(`https://cdn.jsdelivr.net/npm/obook@2.1.37/src/sw/base.js`);
+importScripts(`https://cdn.jsdelivr.net/npm/obook@2.1.38/src/sw/base.js`);
 // importScripts(`http://127.0.0.1:5512/src/sw/base.js`);
